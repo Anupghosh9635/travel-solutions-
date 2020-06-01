ee5624dd3293cc112b5b0af4633003ea6787d4d1
Anup <anupghosh9635@gmail.com>
2020-06-01 11:46:27 +0530

    delete all file

--- src/app/common/AppConstants.ts
+++
@@ -1,8 +0,0 @@
-
-
-export class AppConstants {
-    public static apiURL: string = "http://localhost:8585/";
-    public static siteTitle: string = "This is example of ItSolutionStuff.com";
-    public static headers = new Headers({ 'Content-Type': 'application/json' });
-    
-}
